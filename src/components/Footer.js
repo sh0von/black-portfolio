987b2d59f1d99fee078aa9adda76eaa495d2eca1
shovon <minarshovon909@gmail.com>
2022-09-30 10:28:41 +0000

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,26 +28,7 @@ const Footer = () => {
     <Flex h="12vh" alignItems="center">
       <Flex w="full" justifyContent="flex-end">
         <HStack>
-          <FooterIcon
-            title="twitter"
-            icon={TwitterLogo}
-            link="https://twitter.com/typicalmitul"
-          />
-          <FooterIcon
-            title="instagram"
-            icon={InstagramLogo}
-            link="https://instagram.com/typicalmitul"
-          />
-          <FooterIcon
-            title="github"
-            icon={GithubLogo}
-            link="https://github.com/mitul-s"
-          />
-          <FooterIcon
-            title="linkedin"
-            icon={LinkedinLogo}
-            link="https://linkedin.com/in/heymitul"
-          />
+         
         </HStack>
       </Flex>
     </Flex>
