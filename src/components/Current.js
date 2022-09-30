63b36810b72ad9592dc6f92ad2d297bb265eefd0
shovon <minarshovon909@gmail.com>
2022-09-30 07:13:40 +0000

    first commit

---
+++ src/components/Current.js
@@ -0,0 +1,93 @@
+import {
+  Box,
+  Center,
+  Text,
+  HStack,
+  Icon,
+  Stack,
+  Button,
+  Link,
+  Skeleton,
+} from "@chakra-ui/react";
+import { MusicNotesSimple, Monitor, Bookmarks } from "phosphor-react";
+import Section from "@/components/Section";
+import useSWR from "swr";
+import fetcher from "@/lib/fetcher";
+
+const CurrentItem = ({ icon, title, caption, link }) => {
+  return (
+    <HStack spacing={4}>
+      <Icon fontSize="xl" as={icon} />
+      <Box>
+        <Link href={link} _hover={{ color: "cyan.600" }} isExternal>
+          <Text
+            fontWeight="regular"
+            maxW={["320px", null, "150px"]}
+            minW="120px"
+            whiteSpace="nowrap"
+            overflow="hidden"
+            textOverflow="ellipsis"
+          >
+            {title}
+          </Text>
+          <Text fontSize="sm" color="trueGray.500">
+            {caption}
+          </Text>
+        </Link>
+      </Box>
+    </HStack>
+  );
+};
+
+const CurrentItemSkeleton = ({ icon }) => (
+  <HStack>
+    <Icon fontSize="xl" as={icon} mr={2} />
+    <Center flexDirection="column" minWidth="120px">
+      <Skeleton size="sm" h={6} mb={1} minWidth="120px" />
+      <Skeleton size="sm" h={3} minWidth="120px" />
+    </Center>
+  </HStack>
+);
+
+const Current = () => {
+  const { data: song } = useSWR("/api/now-playing", fetcher);
+  const { data: book } = useSWR("/api/books", fetcher);
+
+  return (
+    <Section header="Currently">
+      <Stack
+        spacing={8}
+        direction={["column", null, "row"]}
+        justifyContent="space-between"
+        // flexShrink="0"
+        flex="1"
+      >
+        {song ? (
+          <CurrentItem
+            icon={MusicNotesSimple}
+            title={song?.title}
+            caption={song?.artist}
+            link={song?.songUrl}
+          />
+        ) : (
+          <CurrentItemSkeleton icon={MusicNotesSimple} />
+        )}
+        
+        <CurrentItem
+          link=""
+          icon={Monitor}
+          title={"Current Project"}
+          caption={"Portfolio"}
+        />
+        <CurrentItem
+          link=""
+          icon={Monitor}
+          title={"Practicing CP"}
+          caption={"Python"}
+        />
+      </Stack>
+    </Section>
+  );
+};
+
+export default Current;
