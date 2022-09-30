b85d1d23f77b8fb2fda3e3de33dbbaf67f9e496d
shovon <minarshovon909@gmail.com>
2022-09-30 12:26:21 +0000

    first commit

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -23,23 +23,20 @@ const AboutText = () => (
       letterSpacing="-0.04px"
     >
       <Text mt={3}>
-        I’m still figuring out what my life would look like here, but for now,
-        I’m happy to share I’m a curious learner of all things. My passions are
-        driven by finding the connections between art, human nature, and numbers
-        to build memorable experiences.
+      I'm still finding out how my life will be here, but I'm delighted to say that I'm an inquisitive learner of everything for the time being. 
       </Text>
       <Box>
         <Text fontWeight="bold" mb={2}>
-          There are two core mantras I like to live my life by:
+        I prefer to live my life by these two main catchphrases: 
         </Text>
         <Stack spacing={2}>
           <HStack>
             <Icon as={ArrowRight} />
-            <Text>Do difficult things because they are the most rewarding</Text>
+            <Text>Do the most challenging tasks because they are the most rewarding. </Text>
           </HStack>
           <HStack>
             <Icon as={ArrowRight} />
-            <Text>Seek discomfort</Text>
+            <Text>Do anything your heart desires. </Text>
           </HStack>
         </Stack>
       </Box>
@@ -48,17 +45,6 @@ const AboutText = () => (
         a B2B SaaS, where I helped convert users from free to paid through
         growth experiments. While in school, I had several internships
         specifically including product roles in banking and operations at Uber.
-        You can learn a little more on my{" "}
-        <Link
-          data-splitbee-event="linkedin"
-          data-splitbee-event-type="Social"
-          href="https://linkedin.com/in/heymitul"
-          textDecoration="underline"
-          _hover={{ color: "cyan.600" }}
-          isExternal
-        >
-          LinkedIn.
-        </Link>
       </Text>
 
       <Text>
@@ -77,23 +63,8 @@ const About = () => {
       <Flex justifyContent="flex-end" flexDirection="column">
         <Stack spacing={4}>
           <Section header="About">
-            <Heading mb={4}>Mitul Shah</Heading>
+            <Heading mb={4}>Shovon</Heading>
             <AboutText />
-            <Text>
-              This is website is always a work in progress - feel free to check
-              it out on{" "}
-              <Link
-                data-splitbee-event="github"
-                data-splitbee-event-type="Social"
-                href="https://github.com/mitul-s/crywolf.me"
-                textDecoration="underline"
-                _hover={{ color: "cyan.600" }}
-                isExternal
-              >
-                GitHub
-              </Link>
-              .
-            </Text>
           </Section>
         </Stack>
       </Flex>
