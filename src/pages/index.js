5a0994da3863697318b3f40227b24aecf56fad62
shovon <minarshovon909@gmail.com>
2022-10-18 02:35:09 +0000

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,16 @@ import Shell from "@/components/Shell";
 import Section from "@/components/Section";
 import Photos from "@/components/Photos";
 import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react"
+import {
   PaperPlaneTilt,
   PaperPlane,
   Code,
@@ -275,6 +285,45 @@ export default function Home() {
           </Stack>
         </Section>
   */}
+
+<Section header="Contact">
+          <Stack spacing={4} mb={7}>
+  <Table size="sm">
+  <Thead>
+    <Tr>
+      <Th>To convert</Th>
+      <Th>into</Th>
+      <Th isNumeric>multiply by</Th>
+    </Tr>
+  </Thead>
+  <Tbody>
+    <Tr>
+      <Td>inches</Td>
+      <Td>millimetres (mm)</Td>
+      <Td isNumeric>25.4</Td>
+    </Tr>
+    <Tr>
+      <Td>feet</Td>
+      <Td>centimetres (cm)</Td>
+      <Td isNumeric>30.48</Td>
+    </Tr>
+    <Tr>
+      <Td>yards</Td>
+      <Td>metres (m)</Td>
+      <Td isNumeric>0.91444</Td>
+    </Tr>
+  </Tbody>
+  <Tfoot>
+    <Tr>
+      <Th>To convert</Th>
+      <Th>into</Th>
+      <Th isNumeric>multiply by</Th>
+    </Tr>
+  </Tfoot>
+</Table>
+
+</Stack>
+        </Section>
      <Photos images={PortfolioImages} mt={-8} mb={8} />
 
         <Section header="Contact">
